1d39383a0b0f88991fde36e16a8df860af80e55c
jeremiahdy55 <jeremiahjava55@gmail.com>
2025-06-16 02:07:29 -1000

    focus on debugging jenkins

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,9 +2,9 @@ output "jenkins_public_ip" {
   value = aws_instance.jenkins.public_ip
 }
 
-output "kafka_public_ip" {
-  value = aws_instance.kafka.public_ip
-}
+# output "kafka_public_ip" {
+#   value = aws_instance.kafka.public_ip
+# }
 
 output "microservices_public_ips" {
   value = { for key, value in aws_instance.microservice : key => value.public_ip }
