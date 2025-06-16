49500570c1080fb0aa4bf0e12104c5f9df55caff
jeremiahdy55 <jeremiahjava55@gmail.com>
2025-06-15 22:13:33 -1000

    added first version of startup script to initialize ec2 instances

--- terraform/ec2_instances.tf
+++ terraform/ec2_instances.tf
@@ -14,11 +14,18 @@ resource "aws_instance" "microservice" {
 #   key_name                    = aws_key_pair.deployer.key_name
   associate_public_ip_address = true
 
+  # Run these commands on creation
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt upgrade -y
+              EOF
+
   tags = {
     Name = each.key
     Role = "microservice"
   }
-  
+
   depends_on = [
     aws_internet_gateway.igw,
     aws_route_table_association.public_assoc
@@ -32,6 +39,37 @@ resource "aws_instance" "jenkins" {
   vpc_security_group_ids      = [aws_security_group.default.id]
 #   key_name                    = aws_key_pair.deployer.key_name
   associate_public_ip_address = true
+
+  # Run these commands on creation TODO fix me later
+  user_data = <<-EOF
+              #!/bin/bash
+              exec > /var/log/jenkins-data.log 2>&1 
+
+              # System update
+              sudo apt update -y
+              sudo apt upgrade -y
+
+              # Install Java 17 for Jenkins to run
+              sudo apt install -y openjdk-17-jdk
+
+               # Add Jenkins repo and import GPG key
+              curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+                /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+              echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+                https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+                /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+              # Install Jenkins
+              apt update -y
+              apt install -y jenkins
+
+              # Enable and start Jenkins
+              systemctl enable jenkins
+              systemctl start jenkins
+
+              sudo 
+              EOF
 
   tags = {
     Name = "Jenkins-Server-fromTF"
@@ -51,6 +89,13 @@ resource "aws_instance" "kafka" {
   vpc_security_group_ids      = [aws_security_group.default.id]
 #   key_name                    = aws_key_pair.deployer.key_name
   associate_public_ip_address = true
+
+  # Run these commands on creation TODO fix me later
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt upgrade -y
+              EOF
 
   tags = {
     Name = "Kafka-Server-fromTF"
