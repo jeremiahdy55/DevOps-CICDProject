062843427cfc5975b0ccc43cf675ea86aae6d0bf
jeremiahdy55 <jeremiahjava55@gmail.com>
2025-06-15 22:33:08 -1000

    added multiple port for each of the microservices to be accessed

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -2,7 +2,7 @@ resource "aws_security_group" "default" {
   name        = "ci-cd-sg"
   description = "Allow SSH, HTTP, and Kafka ports"
   vpc_id = aws_vpc.main.id
-  
+
   ingress {
     description = "SSH"
     from_port   = 22
@@ -28,9 +28,44 @@ resource "aws_security_group" "default" {
   }
 
   ingress {
-    description = "Jenkins UI & ms access"
+    description = "Jenkins UI"
     from_port   = 8080
     to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "ms access: 8081"
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+ingress {
+    description = "ms access: 8082"
+    from_port   = 8082
+    to_port     = 8082
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+ingress {
+    description = "ms access: 8083"
+    from_port   = 8083
+    to_port     = 8083
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+ingress {
+    description = "ms access: 8084"
+    from_port   = 8084
+    to_port     = 8084
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
