c183586e77dda265b11ca919fe7f31f21320aeb0
jeremiahdy55 <jeremiahjava55@gmail.com>
2025-06-16 00:22:19 -1000

    add sudo permissions to wget command

--- terraform/ec2_instances.tf
+++ terraform/ec2_instances.tf
@@ -107,10 +107,10 @@ resource "aws_instance" "kafka" {
 
               # Download the Kafka zip, unzip the file, and move it to the kafka directory and add permissions
               sudo apt install -y wget
-              wget https://archive.apache.org/dist/kafka/3.7.0/kafka_2.13-3.7.0.tgz -O /tmp/kafka.tgz
-              tar -xzf /tmp/kafka.tgz -C /opt
-              mv /opt/kafka_2.13-3.7.0 /opt/kafka
-              chown -R kafka:kafka /opt/kafka
+              sudo wget https://archive.apache.org/dist/kafka/3.7.0/kafka_2.13-3.7.0.tgz -O /tmp/kafka.tgz
+              sudo tar -xzf /tmp/kafka.tgz -C /opt
+              sudo mv /opt/kafka_2.13-3.7.0 /opt/kafka
+              sudo chown -R kafka:kafka /opt/kafka
               
 
               # Start Kafka Zookeeper as kafka user
