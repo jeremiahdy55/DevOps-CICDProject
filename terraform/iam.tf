0c29fd260a4ce09bcee911195f155231194a1e53
jeremiahdy55 <jeremiahjava55@gmail.com>
2025-06-22 15:50:58 -1000

    try deploy microservices using jenkinsfile post build

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -52,3 +52,27 @@ resource "aws_iam_instance_profile" "jenkins_profile" {
   name = "jenkins-ec2-instance-profile"
   role = aws_iam_role.jenkins_role.name
 }
+
+# S3 Bucket access for Jenkins instance
+resource "aws_iam_policy" "s3_access" {
+  name = "jenkins-s3-access"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "s3:GetObject",
+        "s3:PutObject",
+        "s3:DeleteObject"
+      ],
+      Resource = [
+        "${aws_s3_bucket.ci_config_bucket.arn}/*"
+      ]
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "jenkins_s3_access_attach" {
+  role       = aws_iam_role.jenkins_role.name
+  policy_arn = aws_iam_policy.s3_access.arn
+}
